7fa4224b5443c91ac41016c66e29a87aaa3f0034
Bhoomi Gohil <26293804+BhoomiGohil@users.noreply.github.com>
2024-10-01 23:41:02 -0400

    Initial Comment

---
+++ src/Practice17/MainContent.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Body from "./Body";
+
+export default function MainContent() {
+  const [user, setUser] = React.useState("Joe");
+
+  return (
+    <main>
+      <Header user={user} />
+      <Body user={user} />
+    </main>
+  );
+}
